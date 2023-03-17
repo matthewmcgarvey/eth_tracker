0d37146f657850f3602f83cfc56b89f96f04b4c7
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2023-03-16 20:40:10 -0500

    Extract eth price fetching from view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @eth_price = EthPrice.fetch_latest
     @eth_block = EthBlock.last
   end
 end
--- app/models/eth_block.rb
+++ app/models/eth_block.rb
@@ -1,6 +1,6 @@
 class EthBlock < ApplicationRecord
   has_many :eth_transactions
-  after_commit { broadcast_replace_to 'eth_block', target: "eth_block" }
+  after_commit { broadcast_replace_to 'eth_block', target: "eth_block", locals: {eth_price: EthPrice.fetch_latest} }
 
   validates :number, presence: true
 end
